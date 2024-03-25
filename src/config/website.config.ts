bddac28184b93f7051c79fda1e35b8376fb8fbdb
shao <1157663818@qq.com>
2024-03-25 11:17:13 +0800

    重绘开始

---
+++ src/config/website.config.ts
@@ -0,0 +1,17 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: shaowei
+ * @Date: 2024-03-15 09:33:52
+ * @LastEditors: shaowei
+ * @LastEditTime: 2024-03-15 09:33:52
+ */
+import logoImage from '@/assets/images/logo.png';
+import loginImage from '@/assets/images/account-logo.png';
+
+export const websiteConfig = Object.freeze({
+  title: 'uyun',
+  logo: logoImage,
+  loginImage: loginImage,
+  loginDesc: 'uyun_Vue3重绘',
+});
---
+++ src/layout/components/Main/index.vue
@@ -0,0 +1,69 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: shaowei
+ * @Date: 2024-01-15 10:28:23
+ * @LastEditors: shaowei
+ * @LastEditTime: 2024-01-15 10:49:13
+-->
+<template>
+  <RouterView>
+    <template #default="{ Component, route }">
+      <template v-if="mode === 'production'">
+        <transition :name="getTransitionName" mode="out-in" appear>
+          <keep-alive v-if="keepAliveComponents.length" :include="keepAliveComponents">
+            <component :is="Component" :key="route.fullPath" />
+          </keep-alive>
+          <component v-else :is="Component" :key="route.fullPath" />
+        </transition>
+      </template>
+      <template v-else>
+        <keep-alive v-if="keepAliveComponents.length" :include="keepAliveComponents">
+          <component :is="Component" :key="route.fullPath" />
+        </keep-alive>
+        <component v-else :is="Component" :key="route.fullPath" />
+      </template>
+    </template>
+  </RouterView>
+</template>
+
+<script>
+  import { defineComponent, computed, unref } from 'vue';
+  import { useAsyncRouteStore } from '@/store/modules/asyncRoute';
+  import { useProjectSetting } from '@/hooks/setting/useProjectSetting';
+
+  export default defineComponent({
+    name: 'MainView',
+    components: {},
+    props: {
+      notNeedKey: {
+        type: Boolean,
+        default: false,
+      },
+      animate: {
+        type: Boolean,
+        default: true,
+      },
+    },
+    setup() {
+      const { isPageAnimate, pageAnimateType } = useProjectSetting();
+      const asyncRouteStore = useAsyncRouteStore();
+      // 需要缓存的路由组件
+      const keepAliveComponents = computed(() => asyncRouteStore.keepAliveComponents);
+
+      const getTransitionName = computed(() => {
+        return unref(isPageAnimate) ? unref(pageAnimateType) : '';
+      });
+      console.log(getTransitionName);
+
+      const mode = import.meta.env.MODE;
+      return {
+        keepAliveComponents,
+        getTransitionName,
+        mode,
+      };
+    },
+  });
+</script>
+
+<style lang="less" scoped></style>
